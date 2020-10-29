850663cb5f2ecfc52bf3e59ffa9f515e54212585
MIKHAIL GORYUNOV <gormih78@yandex.ru>
2020-10-29 09:36:53 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 import uuid
 
-# Create your models here.
-
 
 class Person(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
